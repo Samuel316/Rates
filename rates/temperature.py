da6ce8412180804f4f946eef749b77daf06a0197
Samuel <samuel.lloyd@ed.ac.uk>
2019-10-22 11:52:41 +0100

    Updated class to convert temp

--- rates/temperature.py
+++ rates/temperature.py
@@ -12,19 +12,29 @@ Return
 """
 
 
-class Temperature:
+class Temperature(float):
     def __init__(self, temperature: [int, float], unit: str = 'Gk'):
+        super().__init__()
         if unit is "Gk":
-            self.gk = temperature
+            super().__init__(temperature)
+            self._kev = None
         elif unit is "KeV":
-            self.gk = temperature * 1.160451e-2
+            super().__init__((temperature * 1.160451e-2))
+            self._kev = temperature
             #  https://physics.nist.gov/cuu/Constants/energy.html
 
         self.unit = 'Gk'
 
     def __str__(self):
-        return str(self.gk) + self.unit
+        return str(self) + self.unit
+
+    @property
+    def kev(self):
+        if self._kev is None:
+            return self / 1.160451e-2
+        elif self._kev is not None:
+            return self._kev
 
 
 if __name__ == "__main__":
-    print(Temperature(30, unit='KeV'))
+    print(Temperature(30, unit='KeV').kev)
